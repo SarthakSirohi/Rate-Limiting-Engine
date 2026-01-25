eb9a949c65d1d22dd2bb4d150b40496059b2db0d
Sarthak Sirohi <your_github_email.com>
2026-01-25 13:14:55 +0530

    Added fail-open if redis is down or unreachable

--- rateLimiter.js
+++ rateLimiter.js
@@ -4,30 +4,37 @@ import { logQueue } from "./queue.js";
 const redisClient = createClient();
 await redisClient.connect();
 
-const WINDOW_SIZE = 60; // seconds
+const WINDOW_SIZE = 60; 
 const MAX_REQUESTS = 5;
 
 export async function rateLimiter(req, res, next) {
   const ip = req.ip;
   const key = `rate:${ip}`;
 
-  const requests = await redisClient.incr(key);
+  try {
+    const requests = await redisClient.incr(key);
 
-  if (requests === 1) {
-    await redisClient.expire(key, WINDOW_SIZE);
+    if (requests === 1) {
+      await redisClient.expire(key, WINDOW_SIZE);
+    }
+
+    if (requests > MAX_REQUESTS) {
+      await logQueue.add("blocked-ip", {
+        ip,
+        time: new Date().toISOString()
+      });
+
+      return res.status(429).json({
+        message: "Too many requests. Try again later."
+      });
+    }
+
+    next();
+  } catch (error) {
+    //  If Redis is down or unreachable
+    console.error("Redis unavailable, skipping rate limit", error.message);
+
+    // FAIL-OPEN: allow request
+    next();
   }
-
-  if (requests > MAX_REQUESTS) {
-    // push log job to queue
-    await logQueue.add("blocked-ip", {
-      ip,
-      time: new Date().toISOString()
-    });
-
-    return res.status(429).json({
-      message: "Too many requests. Try again later."
-    });
-  }
-
-  next();
 }

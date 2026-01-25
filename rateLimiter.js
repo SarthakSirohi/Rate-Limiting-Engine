ec84c7ac5bde9954242b513e37ddb162f678ff3a
Sarthak Sirohi <your_github_email.com>
2026-01-25 13:30:09 +0530

    Add redis based rate limiter with auto IP ban

--- rateLimiter.js
+++ rateLimiter.js
@@ -1,40 +1,58 @@
 import { createClient } from "redis";
-import { logQueue } from "./queue.js";
 
 const redisClient = createClient();
 await redisClient.connect();
 
-const WINDOW_SIZE = 60; 
-const MAX_REQUESTS = 5;
+const WINDOW_SIZE = 60;      // 1 minute
+const MAX_REQUESTS = 20;    // per IP
+
+// AUTO-BAN CONFIG
+const MAX_VIOLATIONS = 10;
+const VIOLATION_WINDOW = 60 * 60;      // 1 hour
+const BAN_DURATION = 60 * 60 * 24;     // 24 hours
 
 export async function rateLimiter(req, res, next) {
   const ip = req.ip;
-  const key = `rate:${ip}`;
 
-  try {
-    const requests = await redisClient.incr(key);
-
-    if (requests === 1) {
-      await redisClient.expire(key, WINDOW_SIZE);
-    }
-
-    if (requests > MAX_REQUESTS) {
-      await logQueue.add("blocked-ip", {
-        ip,
-        time: new Date().toISOString()
-      });
-
-      return res.status(429).json({
-        message: "Too many requests. Try again later."
-      });
-    }
-
-    next();
-  } catch (error) {
-    //  If Redis is down or unreachable
-    console.error("Redis unavailable, skipping rate limit", error.message);
-
-    // FAIL-OPEN: allow request
-    next();
+  // Check if IP is banned
+  if (await redisClient.exists(`ban:${ip}`)) {
+    return res.status(403).json({
+      message: "IP banned due to excessive abuse"
+    });
   }
+
+  const rateKey = `rate:${ip}`;
+  const requests = await redisClient.incr(rateKey);
+
+  // Start rate limit window
+  if (requests === 1) {
+    await redisClient.expire(rateKey, WINDOW_SIZE);
+  }
+
+  // Rate limit exceeded
+  if (requests > MAX_REQUESTS) {
+    const violationKey = `violation:${ip}`;
+    const violations = await redisClient.incr(violationKey);
+
+    if (violations === 1) {
+      await redisClient.expire(violationKey, VIOLATION_WINDOW);
+    }
+
+    // Auto-ban IP
+    if (violations >= MAX_VIOLATIONS) {
+      await redisClient.set(`ban:${ip}`, "1", {
+        EX: BAN_DURATION
+      });
+
+      return res.status(403).json({
+        message: "IP banned for 24 hours"
+      });
+    }
+
+    return res.status(429).json({
+      message: "Too many requests"
+    });
+  }
+
+  next();
 }
